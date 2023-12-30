a15fdc2bd6e547cf45375850dca5f4a37e74e819
yashwant <gurjaryash00@gmail.com>
2023-12-30 22:02:31 +0530

    my first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+Tic-Tac-Toe Game:
+This is a simple Tic-Tac-Toe game built with plain HTML, CSS, and JavaScript. It allows two players to play the classic game of Tic-Tac-Toe against each other.
+
+Features:
+Two-player gameplay for Tic-Tac-Toe.
+Interactive game board for making moves.
+Determines the winner or if it's a draw.
+
+How to Play:
+Clone the repository or download the ZIP file.
+Open the index.html file in your web browser.
+Players take turns clicking on the game board to make their moves.
+The game determines the winner when a player gets three in a row horizontally, vertically, or diagonally.
+If all spaces are filled and no player achieves three in a row, the game ends in a draw.
+
+Project Structure:
+/index.html: Main HTML file containing the structure of the Tic-Tac-Toe game.
+/index.css: Contains the styling for the game interface.
+/app.js: JavaScript file with functionality for the game logic.
---
+++ app.js
@@ -0,0 +1,78 @@
+let boxes = document.querySelectorAll('.box');
+let rstButton = document.querySelector('#reset-btn');
+let newbtn = document.querySelector('#new-btn');
+let msgcontainer = document.querySelector('.msg-container')
+let msg = document.querySelector('#msg')
+
+let turn0 = true;
+let winningPaterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+]
+
+boxes.forEach((box)=>{
+    box.addEventListener('click', ()=> {
+        box.innerHTML = 'yash' 
+        if(turn0){
+            box.innerHTML = 'O';
+            turn0 = false
+        }else{
+            box.innerHTML = 'X';
+            turn0 = true
+        }
+        box.disabled = true
+
+        checkWinner()
+    },false)
+    
+})
+
+function resetBtn(){
+    turn0 = true;
+    enableBoxes()
+    msgcontainer.classList.add('hide')
+}
+
+function dibledBoxes(){
+    for(let box of boxes){
+        box.disabled = true;
+    }
+}
+
+function enableBoxes(){
+    for(let box of boxes){
+        box.disabled = false;
+        box.innerHTML = "";
+    }
+}
+
+function showWinner(winner){
+    msg.innerText = `Congratulations! The Winner is ${winner}`
+    msgcontainer.classList.remove("hide")
+    dibledBoxes()
+}
+
+function checkWinner(){
+    for(let pattern of winningPaterns){
+        // console.log(pattern[0], pattern[1], pattern[2])
+        // console.log(boxes[pattern[0]], boxes[pattern[1]], boxes[pattern[2]])
+       let firstval = boxes[pattern[0]].innerText;
+       let secndval = boxes[pattern[1]].innerText;
+       let thirdval = boxes[pattern[2]].innerText
+       if(firstval != "" && secndval != "" && thirdval != ""){
+        if(firstval == secndval && secndval == thirdval){
+            console.log("winner")
+            showWinner(firstval)
+        }
+       }
+    }
+}
+
+rstButton.addEventListener('click', resetBtn)
+newbtn.addEventListener("click", resetBtn)
